36c1dd251f13cf708a11ba6c3c16a00cdf525e5e
Cristhiam Reina <cristiansrc@gmail.com>
2026-03-09 22:37:27 -0500

    Create project

---
+++ src/test/kotlin/com/laura/aviles/ms/ticket/users/MsTicketUsersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.laura.aviles.ms.ticket.users
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsTicketUsersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
